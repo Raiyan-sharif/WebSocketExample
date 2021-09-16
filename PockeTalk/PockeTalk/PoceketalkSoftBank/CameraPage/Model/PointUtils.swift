b4d6afcf88e01e97942a09b82513263aeb0c8cd3
Hajjaz Bin Ibrahim <hajjaz.ibrahim@bjitgroup.com>
2021-09-16 19:14:57 +0600

    [PTS-iOS]PT_SK-7423] [Camera Image processing integration for block and line mode in iOS
    - point calculation error fixed
    - Line vertices calculation error fixed
    
    ref: https://review2.bjitgroup.com:8443/c/p1406.2_pocketalk_smartphone_ios/+/112653
    
    Change-Id: If8ff09265c103eab2be2ca8d54288cdcd57070e6

--- PockeTalk/PockeTalk/PoceketalkSoftBank/CameraPage/Model/PointUtils.swift
+++ PockeTalk/PockeTalk/PoceketalkSoftBank/CameraPage/Model/PointUtils.swift
@@ -26,34 +26,34 @@ public class PointUtils {
                     var blockText: String = ""
                     var listLanguageCodes: Array<String> = []
                     var x1:Int = 0, y1:Int = 0, x2:Int = 0, y4:Int = 0, x3:Int = 0, x4:Int = 0, y2:Int = 0, y3:Int = 0
-                    if let verticeX1 = block.boundingBox?.vertices!{
-                        x1 = nonMinusPoint(Int(Float(verticeX1[0].x!) * xFactor))
+                    if let verticeX1 = block.boundingBox?.vertices![0].x{
+                        x1 = nonMinusPoint(Int(Float(verticeX1) * xFactor))
                     }
-                    if let verticeY1 = block.boundingBox?.vertices!{
-                        y1 = nonMinusPoint(Int(Float(verticeY1[0].y!) * xFactor))
+                    if let verticeY1 = block.boundingBox?.vertices![0].y{
+                        y1 = nonMinusPoint(Int(Float(verticeY1) * yFactor))
                     }
-                    if let verticeX2 = block.boundingBox?.vertices!{
-                        x2 = nonMinusPoint(Int(Float(verticeX2[1].x!) * xFactor))
+                    if let verticeX2 = block.boundingBox?.vertices![1].x{
+                        x2 = nonMinusPoint(Int(Float(verticeX2) * xFactor))
                     }
-                    if let verticeY2 = block.boundingBox?.vertices!{
-                        y2 = nonMinusPoint(Int(Float(verticeY2[1].y!) * xFactor))
+                    if let verticeY2 = block.boundingBox?.vertices![1].y{
+                        y2 = nonMinusPoint(Int(Float(verticeY2) * yFactor))
                     }
-                    if let verticeX3 = block.boundingBox?.vertices!{
-                        x3 = nonMinusPoint(Int(Float(verticeX3[2].x!) * xFactor))
+                    if let verticeX3 = block.boundingBox?.vertices![2].x{
+                        x3 = nonMinusPoint(Int(Float(verticeX3) * xFactor))
                     }
-                    if let verticeY3 = block.boundingBox?.vertices!{
-                        y3 = nonMinusPoint(Int(Float(verticeY3[2].y!) * xFactor))
+                    if let verticeY3 = block.boundingBox?.vertices![2].y{
+                        y3 = nonMinusPoint(Int(Float(verticeY3) * yFactor))
                     }
-                    if let verticeX4 = block.boundingBox?.vertices!{
-                        x4 = nonMinusPoint(Int(Float(verticeX4[3].x!) * xFactor))
+                    if let verticeX4 = block.boundingBox?.vertices![3].x{
+                        x4 = nonMinusPoint(Int(Float(verticeX4) * xFactor))
                     }
-                    if let verticeY4 = block.boundingBox?.vertices!{
-                        y4 = nonMinusPoint(Int(Float(verticeY4[3].y!) * xFactor))
+                    if let verticeY4 = block.boundingBox?.vertices![3].y{
+                        y4 = nonMinusPoint(Int(Float(verticeY4) * yFactor))
                     }
                     var blockText1: String = "(x1,y1)(\(x1),\(y1))(x2,y2)(\(x2),\(y2))(x3,y3)(\(x3),\(y3))(x4,y4)(\(x4),\(y4))"
                     let blockBoundingBoxes = BoundingBoxs(vertices: [Vertexs(x: x1, y: y1), Vertexs(x: x2, y:y2), Vertexs(x:x3,y:y3), Vertexs(x:x4, y:y4)])
                     
-                    PrintUtility.printLog(tag: " Bounding Box: ", text: "\(blockText1)")
+                    //PrintUtility.printLog(tag: " Bounding Box: ", text: "\(blockText1)")
                     
                     if(isVerticalBlock(CGPoint(x:x1, y:y1), CGPoint(x: x2, y: y2))){
                         if isBottomTop(y1, y2){
@@ -138,31 +138,37 @@ public class PointUtils {
                             var wordText: String = "", breakStr: String = "", strBreak: String = ""
                             let symbols = word.symbols
                             for symbol in symbols!{
+                                
+                                //var blockText2: String = "(x1,y1)(\(x1),\(y1))(x2,y2)(\(x2),\(y2))(x3,y3)(\(x3),\(y3))(x4,y4)(\(x4),\(y4))"
+                                //PrintUtility.printLog(tag: "Value of points: ", text: blockText2)
+                                
                                 if(x1==0 && y1 == 0 && x4 == 0 && y4 == 0 && x2 == 0 && y2 == 0 && x3 == 0 && y3 == 0) {
-                                    if let verticeX1 = block.boundingBox?.vertices!{
-                                        x1 = nonMinusPoint(Int(Float(verticeX1[0].x!) * xFactor))
+                                    if let verticeX1 = symbol.boundingBox?.vertices![0].x{
+                                        x1 = nonMinusPoint(Int(Float(verticeX1) * xFactor))
                                     }
-                                    if let verticeY1 = block.boundingBox?.vertices!{
-                                        y1 = nonMinusPoint(Int(Float(verticeY1[0].y!) * xFactor))
+                                    if let verticeY1 = symbol.boundingBox?.vertices![0].y{
+                                        y1 = nonMinusPoint(Int(Float(verticeY1) * yFactor))
                                     }
-                                    if let verticeX2 = block.boundingBox?.vertices!{
-                                        x2 = nonMinusPoint(Int(Float(verticeX2[1].x!) * xFactor))
+                                    if let verticeX2 = symbol.boundingBox?.vertices![1].x{
+                                        x2 = nonMinusPoint(Int(Float(verticeX2) * xFactor))
                                     }
-                                    if let verticeY2 = block.boundingBox?.vertices!{
-                                        y2 = nonMinusPoint(Int(Float(verticeY2[1].y!) * xFactor))
+                                    if let verticeY2 = symbol.boundingBox?.vertices![1].y{
+                                        y2 = nonMinusPoint(Int(Float(verticeY2) * yFactor))
                                     }
-                                    if let verticeX3 = block.boundingBox?.vertices!{
-                                        x3 = nonMinusPoint(Int(Float(verticeX3[2].x!) * xFactor))
+                                    if let verticeX3 = symbol.boundingBox?.vertices![2].x{
+                                        x3 = nonMinusPoint(Int(Float(verticeX3) * xFactor))
                                     }
-                                    if let verticeY3 = block.boundingBox?.vertices!{
-                                        y3 = nonMinusPoint(Int(Float(verticeY3[2].y!) * xFactor))
+                                    if let verticeY3 = symbol.boundingBox?.vertices![2].y{
+                                        y3 = nonMinusPoint(Int(Float(verticeY3) * yFactor))
                                     }
-                                    if let verticeX4 = block.boundingBox?.vertices!{
-                                        x4 = nonMinusPoint(Int(Float(verticeX4[3].x!) * xFactor))
+                                    if let verticeX4 = symbol.boundingBox?.vertices![3].x{
+                                        x4 = nonMinusPoint(Int(Float(verticeX4) * xFactor))
                                     }
-                                    if let verticeY4 = block.boundingBox?.vertices!{
-                                        y4 = nonMinusPoint(Int(Float(verticeY4[3].y!) * xFactor))
+                                    if let verticeY4 = symbol.boundingBox?.vertices![3].y{
+                                        y4 = nonMinusPoint(Int(Float(verticeY4) * yFactor))
                                     }
+                                    var blockText3: String = "(x1,y1)(\(x1),\(y1))(x2,y2)(\(x2),\(y2))(x3,y3)(\(x3),\(y3))(x4,y4)(\(x4),\(y4))"
+                                    PrintUtility.printLog(tag: "Starting points: ", text: blockText3)
                                 }
                                 wordText = wordText + symbol.text!
                                 if let detectedBreak = symbol.property?.detectedBreak{
@@ -172,58 +178,66 @@ public class PointUtils {
                                 }
                                 if(getBreakType(breakStr).contains("\n")) {
                                     var lastX1:Int = 0, lastY1:Int = 0;
-                                    if let verticeX1 = block.boundingBox?.vertices!{
-                                        lastX1 = nonMinusPoint(Int(Float(verticeX1[0].x!) * xFactor))
+                                    if let verticeX1 = symbol.boundingBox?.vertices![0].x{
+                                        lastX1 = nonMinusPoint(Int(Float(verticeX1) * xFactor))
                                     }
-                                    if let verticeY1 = block.boundingBox?.vertices!{
-                                        lastY1 = nonMinusPoint(Int(Float(verticeY1[0].y!) * xFactor))
+                                    if let verticeY1 = symbol.boundingBox?.vertices![0].y{
+                                        lastY1 = nonMinusPoint(Int(Float(verticeY1) * yFactor))
                                     }
+                                    //let a = area(x1, y1, x4, y4, lastX1, lastY1)
+                                    //let b = area(x1, y1, x2, y2, lastX1, lastY1)
+                                    //print("area(x1, y1, x4, y4, lastX1, lastY1): \(a) area(x1, y1, x2, y2, lastX1, lastY1): \(b)")
                                     if (area(x1, y1, x4, y4, lastX1, lastY1) == (area(x1, y1, x2, y2, lastX1, lastY1))) {
+                                        // top-to-bottom text
                                         if (distanceBetweenPoints(CGPoint(x:x1, y:y1), CGPoint(x:lastX1, y:lastY1)) < distanceBetweenPoints(CGPoint(x:x2, y:y2), CGPoint(x:lastX1, y:lastY1))) {
                                             x1 = lastX1
                                             y1 = lastY1
-                                            if let verticeX4 = block.boundingBox?.vertices!{
-                                                x4 = nonMinusPoint(Int(Float(verticeX4[3].x!) * xFactor))
+                                            if let verticeX4 = symbol.boundingBox?.vertices![3].x{
+                                                x4 = nonMinusPoint(Int(Float(verticeX4) * xFactor))
                                             }
-                                            if let verticeY4 = block.boundingBox?.vertices!{
-                                                y4 = nonMinusPoint(Int(Float(verticeY4[3].y!) * xFactor))
+                                            if let verticeY4 = symbol.boundingBox?.vertices![3].y{
+                                                y4 = nonMinusPoint(Int(Float(verticeY4) * yFactor))
                                             }
                                         } else{
-                                            if let verticeX2 = block.boundingBox?.vertices!{
-                                                x2 = nonMinusPoint(Int(Float(verticeX2[1].x!) * xFactor))
+                                            if let verticeX2 = symbol.boundingBox?.vertices![1].x{
+                                                x2 = nonMinusPoint(Int(Float(verticeX2) * xFactor))
                                             }
-                                            if let verticeY2 = block.boundingBox?.vertices!{
-                                                y2 = nonMinusPoint(Int(Float(verticeY2[1].y!) * xFactor))
+                                            if let verticeY2 = symbol.boundingBox?.vertices![1].y{
+                                                y2 = nonMinusPoint(Int(Float(verticeY2) * yFactor))
                                             }
-                                            if let verticeX3 = block.boundingBox?.vertices!{
-                                                x3 = nonMinusPoint(Int(Float(verticeX3[2].x!) * xFactor))
+                                            if let verticeX3 = symbol.boundingBox?.vertices![2].x{
+                                                x3 = nonMinusPoint(Int(Float(verticeX3) * xFactor))
                                             }
-                                            if let verticeY3 = block.boundingBox?.vertices!{
-                                                y3 = nonMinusPoint(Int(Float(verticeY3[2].y!) * xFactor))
+                                            if let verticeY3 = symbol.boundingBox?.vertices![2].y{
+                                                y3 = nonMinusPoint(Int(Float(verticeY3) * yFactor))
                                             }
                                         }
+                                        var blockText4: String = "(x1,y1)(\(x1),\(y1))(x2,y2)(\(x2),\(y2))(x3,y3)(\(x3),\(y3))(x4,y4)(\(x4),\(y4))"
+                                        PrintUtility.printLog(tag: "End points if: ", text: blockText4)
                                     } else {
-                                        // top-to-bottom text
-                                        x4 = x1
-                                        y4 = y1
-                                        x1 = x2
-                                        y1 = y2
-                                        if let verticeX3 = block.boundingBox?.vertices!{
-                                            x2 = nonMinusPoint(Int(Float(verticeX3[2].x!) * xFactor))
+                                        // left-to-right, bottom-to-top text
+//                                        x4 = x1
+//                                        y4 = y1
+//                                        x1 = x2
+//                                        y1 = y2
+                                        if let verticeX3 = symbol.boundingBox?.vertices![2].x{
+                                            x2 = nonMinusPoint(Int(Float(verticeX3) * xFactor))
                                         }
-                                        if let verticeY3 = block.boundingBox?.vertices!{
-                                            y2 = nonMinusPoint(Int(Float(verticeY3[2].y!) * xFactor))
+                                        if let verticeY3 = symbol.boundingBox?.vertices![2].y{
+                                            y2 = nonMinusPoint(Int(Float(verticeY3) * yFactor))
                                         }
-                                        if let verticeX4 = block.boundingBox?.vertices!{
-                                            x3 = nonMinusPoint(Int(Float(verticeX4[3].x!) * xFactor))
+                                        if let verticeX4 = symbol.boundingBox?.vertices![3].x{
+                                            x3 = nonMinusPoint(Int(Float(verticeX4) * xFactor))
                                         }
-                                        if let verticeY4 = block.boundingBox?.vertices!{
-                                            y3 = nonMinusPoint(Int(Float(verticeY4[3].y!) * xFactor))
+                                        if let verticeY4 = symbol.boundingBox?.vertices![3].y{
+                                            y3 = nonMinusPoint(Int(Float(verticeY4) * yFactor))
                                         }
+                                        var blockText4: String = "(x1,y1)(\(x1),\(y1))(x2,y2)(\(x2),\(y2))(x3,y3)(\(x3),\(y3))(x4,y4)(\(x4),\(y4))"
+                                        PrintUtility.printLog(tag: "End points else: ", text: blockText4)
                                     }
                                     var blockText1: String = "(x1,y1)(\(x1),\(y1))(x2,y2)(\(x2),\(y2))(x3,y3)(\(x3),\(y3))(x4,y4)(\(x4),\(y4))"
                                     let blockBoundingBoxes = BoundingBoxs(vertices: [Vertexs(x: x1, y: y1), Vertexs(x: x2, y:y2), Vertexs(x:x3,y:y3), Vertexs(x:x4, y:y4)])
-                                    //print("Bounding Box: \(blockText1)")
+                                    PrintUtility.printLog(tag: "Bounding Box:",text: blockText1)
                                     if(isVerticalBlock(CGPoint(x:x1, y:y1), CGPoint(x: x2, y: y2))){
                                         if isBottomTop(y1, y2){
                                             bottomTopBlock = intBlockIndex
@@ -253,7 +267,7 @@ public class PointUtils {
                                     intBlockIndex += intBlockIndex
                                     paraText = ""
                                     wordText = ""
-                                    break;
+                                    break
                                 }
                                 if let detectedLanguage = symbol.property?.detectedLanguages {
                                     if let languageCode: String = detectedLanguage[0].languageCode {
@@ -263,14 +277,14 @@ public class PointUtils {
                                     }
                                 }
                             }
-                            strBreak = getBreakType(breakStr);
+                            strBreak = getBreakType(breakStr)
                             if(strBreak != "\n") {
-                                paraText = paraText + wordText + strBreak;
+                                paraText = paraText + wordText + strBreak
                             }
                         }
-                        blockText = blockText + paraText;
+                        blockText = blockText + paraText
                     }
-                    pageText = pageText + blockText;
+                    pageText = pageText + blockText
                     blockClass = BlockClass(languageCodeFrom: mDetectedLanguageCode, blocks: arrBlockElement)
                 }
             }
