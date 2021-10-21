3482408ae4903ee7108d87e811ba57efba9c9d14
Muhammad Ariful Islam <muhammad.ariful@bjitgroup.com>
2021-10-21 13:24:30 +0600

    PT_SK-7841 [Issue Fix] STT screen Guide Text is not showing after accepting microphone permission for first time
    	- STT guide text not showing after accepting microphone permission for the first time.
    	- Code refactoring in SpeechProcessingViewController & others.
    	- Resolve merge conflict.
    
    Ref: https://sourcenext.backlog.com/view/PT_SK-7841
    
    Change-Id: I578d17924db211832757bb51e3308992821f8a71

---
+++ PockeTalk/PockeTalk/Extensions/UIExtensions/Extension+UIApplication.swift
@@ -0,0 +1,24 @@
+//
+//  Extension+UIApplication.swift
+//  PockeTalk
+//
+//  Created by BJIT on 21/10/21.
+//
+
+import UIKit
+
+extension UIApplication {
+    class func getTopViewController(base: UIViewController? = UIApplication.shared.keyWindow?.rootViewController) -> UIViewController? {
+
+        if let nav = base as? UINavigationController {
+            return getTopViewController(base: nav.visibleViewController)
+
+        } else if let tab = base as? UITabBarController, let selected = tab.selectedViewController {
+            return getTopViewController(base: selected)
+
+        } else if let presented = base?.presentedViewController {
+            return getTopViewController(base: presented)
+        }
+        return base
+    }
+}
