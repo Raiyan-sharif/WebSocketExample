2b8c8951fd1178f41ec60b2af627a4f01eecb8ed
shadikul.bari <shadikul.bari@bjitgroup.com>
2021-10-25 15:04:11 +0600

    [PT_SK-7881][SoftBank] Fix title issue for "Reset" dialog.
    https://sourcenext.backlog.com/view/PT_SK-7881

--- PockeTalk/PockeTalk/Reusable/Alert/ViewModel/CustomAlertViewModel.swift
+++ PockeTalk/PockeTalk/Reusable/Alert/ViewModel/CustomAlertViewModel.swift
@@ -12,7 +12,7 @@ class CustomAlertViewModel:BaseModel {
     func alertDialogWithoutTitleWithActionButton(message:String, buttonTitle:String, completion:@escaping () -> Void) -> CustomAlertDailogViewController {
         let storyboard = UIStoryboard(name: "CustomAlertDialog", bundle: .main)
         let alertVC = storyboard.instantiateViewController(withIdentifier: "CustomAlertVC") as! CustomAlertDailogViewController
-        alertVC.alertTitle = "title"
+        //alertVC.alertTitle = "title"
         alertVC.alertMessage = message
         alertVC.alertButton = buttonTitle
         alertVC.noTitleShown = true
@@ -25,7 +25,7 @@ class CustomAlertViewModel:BaseModel {
     func alertDialogWithoutTitleWithoutActionButton(message:String) -> CustomAlertDailogViewController {
         let storyboard = UIStoryboard(name: "CustomAlertDialog", bundle: .main)
         let alertVC = storyboard.instantiateViewController(withIdentifier: "CustomAlertVC") as! CustomAlertDailogViewController
-        alertVC.alertTitle = "title"
+        //alertVC.alertTitle = "title"
         alertVC.alertMessage = message
         alertVC.alertButton = "buttonTitle"
         alertVC.noTitleShown = true
