f88a219361054ce28b6c48f95d2cb03649ad9a13
Muhammad Ariful Islam <muhammad.ariful@bjitgroup.com>
2022-03-02 20:16:54 +0600

    [PT-SB-iOS] Remove navigation bar from initial loader view
     ✔ Remove initial bar when loader view appear.
    
    Refs:
    https://sourcenext.backlog.com/view/PT_SK-9032

--- PockeTalk/PockeTalk/PoceketalkSoftBank/WelcomeAndPayment/IAPStatusCheckDummyLoadingViewController/IAPStatusCheckDummyLoadingViewController.swift
+++ PockeTalk/PockeTalk/PoceketalkSoftBank/WelcomeAndPayment/IAPStatusCheckDummyLoadingViewController/IAPStatusCheckDummyLoadingViewController.swift
@@ -14,6 +14,7 @@ class IAPStatusCheckDummyLoadingViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         activityIndicator.startAnimating()
+        self.navigationController?.navigationBar.isHidden = true
         // Do any additional setup after loading the view.
     }
     
