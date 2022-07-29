a87a9b739e000b455d676e89f43350cc6011db37
Mohammed Nasif Zawad <nasif.zawad@bjitgroup.com>
2022-07-29 18:04:33 +0600

    [PTiOS] [Observation] [Existing] [Reset Screen] Mismatch in letter case comparing with PT app and android app.
    - Fixed "Delete the Camera translation history" to "Delete the camera translation history"
    - Where change the Capital C to small c
    
    
    
    Ref: https://sourcenext.backlog.com/view/PT_SK-10307#comment-175089564

--- PockeTalk/PockeTalk/PoceketalkSoftBank/Settings/Reset/Model/ResetItems.swift
+++ PockeTalk/PockeTalk/PoceketalkSoftBank/Settings/Reset/Model/ResetItems.swift
@@ -7,7 +7,7 @@ import Foundation
 
 enum ResetItemType: String, CaseIterable {
     case clearTranslationHistory = "Clear translation history"
-    case deleteCameraHistory = "Delete the Camera translation history"
+    case deleteCameraHistory = "Delete the camera translation history"
     case clearFavourite = "Clear all Favorite"
     case deleteAllData = "Delete all the data"
     static var resetItems: [String] {
