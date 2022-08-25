39d38c67d8f2a1f1b25786412f316c6a99305f3c
Muhammad Ariful Islam <muhammad.ariful@bjitgroup.com>
2022-08-25 14:05:00 +0600

    [PT-SB-iOS-Labo] Camera Language select by voice for To language is not working for the very first time
    ✔ Resolve camera Language select by voice for To language is not working for the very first time.
    
    Ref: https://sourcenext.backlog.com/view/PT_SK-10464
    
    Change-Id: I4ff59a4498791d5c717e088afdafd7db370e486b

--- PockeTalk/PockeTalk/PoceketalkSoftBank/CameraPage/ViewController/CameraViewController.swift
+++ PockeTalk/PockeTalk/PoceketalkSoftBank/CameraPage/ViewController/CameraViewController.swift
@@ -14,7 +14,6 @@ enum Camera {
 }
 
 class CameraViewController: BaseViewController, AVCapturePhotoCaptureDelegate {
-    
     let TAG = CameraViewController.self
     var minimumSize: CGSize = CGSize(width: 60, height: 60)
     @IBOutlet weak var zoomLevel: UILabel!
@@ -29,7 +28,7 @@ class CameraViewController: BaseViewController, AVCapturePhotoCaptureDelegate {
     let window = UIApplication.shared.keyWindow!
     var talkButtonImageView: UIImageView!
     var isLanguageViewPresent = false
-    
+
     @IBOutlet weak var captureButton: UIButton!
     @IBOutlet weak var toLangLabel: MarqueeLabel!
     @IBOutlet weak var fromLangLabel: MarqueeLabel!
@@ -45,7 +44,7 @@ class CameraViewController: BaseViewController, AVCapturePhotoCaptureDelegate {
     private lazy var previewView = UIView()
     private lazy var previewLayer = AVCaptureVideoPreviewLayer(session: session)
     private let currentDevice = UIDevice.current
-    
+
     var cropImageRect = CGRect()
     open var onCompletion: CameraViewCompletion?
     var zoomScaleRange: ClosedRange<CGFloat> = 1...5
@@ -53,7 +52,7 @@ class CameraViewController: BaseViewController, AVCapturePhotoCaptureDelegate {
     var sessionSetupSucceeds = false
     private var captureProcessors: [Int64: PhotoCaptureProcessor] = [:]
     private var videoOrientation: AVCaptureVideoOrientation = .portrait
-    
+
     /// Circle drawing properties
     let startAngle : CGFloat = 0
     let endAngle : CGFloat = CGFloat(Double.pi * 2)
@@ -63,13 +62,12 @@ class CameraViewController: BaseViewController, AVCapturePhotoCaptureDelegate {
     let zoomLabelBorderWidth : CGFloat = 2.0
     var circleColor: UIColor = .white
     var zoomLevelGoogleAnalytic : Float = 1.0
-    
+
     /// Camera History
     private let cameraHistoryViewModel = CameraHistoryViewModel()
     var updateHomeContainer:((_ isTalkButtonVisible:Bool)->())?
-
     private var connectivity = Connectivity()
-    
+
     @IBAction func onFromLangBtnPressed(_ sender: Any) {
         buttonFromLanguageLogEvent()
         self.updateHomeContainer?(false)
@@ -79,7 +77,7 @@ class CameraViewController: BaseViewController, AVCapturePhotoCaptureDelegate {
         let index = UserDefaultsProperty<Int>(kCameraFromLanguageSelectionIndex).value
         setCameraLanguageScreenTracker(index: index ?? 0)
     }
-    
+
     @IBAction func onTargetLangBtnPressed(_ sender: Any) {
         buttonTargetLanguageLogEvent()
         self.updateHomeContainer?(false)
@@ -87,19 +85,19 @@ class CameraViewController: BaseViewController, AVCapturePhotoCaptureDelegate {
         UserDefaultsProperty<Bool>(KCameraLanguageFrom).value = false
         openCameraLanguageListScreen()
         let index = UserDefaultsProperty<Int>(kCameraToLanguageSelectionIndex).value
-        setCameraLanguageScreenTracker(index: index ?? 1)
+        setCameraLanguageScreenTracker(index: index ?? 0)
     }
 
     private func setCameraLanguageScreenTracker(index : Int) {
         index == 1 ? (ScreenTracker.sharedInstance.screenPurpose = .LanguageHistorySelectionCamera) : (ScreenTracker.sharedInstance.screenPurpose = .LanguageSelectionCamera)
     }
-    
+
     func openCameraLanguageListScreen(){
         isViewInteractionEnable(false)
         let storyboard = UIStoryboard(name: KStoryBoardCamera, bundle: nil)
         let controller = storyboard.instantiateViewController(withIdentifier: KiDLangSelectCamera)as! LanguageSelectCameraVC
         let transition = GlobalMethod.addMoveInTransitionAnimatation(duration: kScreenTransitionTime, animationStyle: CATransitionSubtype.fromRight)
-        
+
         add(asChildViewController: controller, containerView: view, animation: transition)
         guard let activeCamera = activeCamera else {
             return
@@ -112,7 +110,7 @@ class CameraViewController: BaseViewController, AVCapturePhotoCaptureDelegate {
             self?.talkButtonImageView.isHidden = false
         }
     }
-    
+
     fileprivate func updateLanguageNames() {
         let languageManager = LanguageSelectionManager.shared
         let fromLangCode = CameraLanguageSelectionViewModel.shared.fromLanguage
@@ -123,7 +121,7 @@ class CameraViewController: BaseViewController, AVCapturePhotoCaptureDelegate {
         } else {
             UserDefaultsProperty<String>(KCameraTempTargetLanguage).value = targetLangCode
         }
-        
+
         let fromLang = CameraLanguageSelectionViewModel.shared.getLanguageInfoByCode(langCode: fromLangCode, languageList: CameraLanguageSelectionViewModel.shared.getFromLanguageLanguageList())
         let targetLang = languageManager.getLanguageInfoByCode(langCode: targetLangCode)
 
@@ -137,7 +135,7 @@ class CameraViewController: BaseViewController, AVCapturePhotoCaptureDelegate {
         }
         toLangLabel.text = "\(targetLang?.sysLangName ?? "") (\(targetLang?.name ?? ""))"
     }
-    
+
     @objc func onCameraLanguageChanged(notification: Notification) {
         updateLanguageNames()
         isViewInteractionEnable(true)
@@ -149,22 +147,22 @@ class CameraViewController: BaseViewController, AVCapturePhotoCaptureDelegate {
         isLanguageViewPresent = false
         circleColor = .white
     }
-    
+
     func registerNotification(){
         NotificationCenter.default.addObserver(self, selector: #selector(self.onCameraLanguageChanged(notification:)), name: .languageSelectionCameraNotification, object: nil)
-        
+
         NotificationCenter.default.addObserver(self, selector: #selector(applicationDidBecomeActive), name: UIApplication.didBecomeActiveNotification, object: nil)
     }
-    
+
     func unregisterNotification(){
         NotificationCenter.default.removeObserver(self, name: .languageSelectionCameraNotification, object: nil)
     }
-    
+
     deinit {
         connectivity.cancel()
         unregisterNotification()
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
 //        AppDelegate.executeLicenseTokenRefreshFunctionality(){ result in }
@@ -208,19 +206,19 @@ class CameraViewController: BaseViewController, AVCapturePhotoCaptureDelegate {
                                                name: .AVCaptureSessionWasInterrupted,
                                                object: session)
     }
-    
+
     @objc
     func didInterrupted() {
         PrintUtility.printLog(tag: "Session", text: "\(session.isInterrupted)")
     }
-    
+
     @objc func applicationDidBecomeActive() {
         guard let isCameraFlashOn: Bool = UserDefaults.standard.value(forKey: isCameraFlashOn) as? Bool else {return}
         if isCameraFlashOn == true && isLanguageViewPresent == false {
             turnOnCameraFlash()
         }
     }
-    
+
     func turnOnCameraFlash() {
         if let activeCamera = activeCamera {
             if activeCamera.hasTorch {
@@ -244,30 +242,28 @@ class CameraViewController: BaseViewController, AVCapturePhotoCaptureDelegate {
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
     }
-    
+
     func setUPViews() {
         changeStatusBarColor()
         let tap = UITapGestureRecognizer(target: self, action: #selector(imageHistoryEvent(sender: )))
         self.cameraHistoryImageView.addGestureRecognizer(tap)
-        
+
         /// Make zoom label round shaped
         self.zoomLevel.layer.masksToBounds = true
         self.zoomLevel.layer.cornerRadius = self.zoomLevel.frame.size.width/2
         self.zoomLevel.layer.borderWidth = zoomLabelBorderWidth
         self.zoomLevel.layer.borderColor = UIColor.white.cgColor
     }
-    
+
     func changeStatusBarColor() {
         let app = UIApplication.shared
         let statusBarHeight: CGFloat = app.statusBarFrame.size.height
-        
+
         let statusbarView = UIView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.size.width, height: statusBarHeight))
         statusbarView.backgroundColor = UIColor.black
         view.addSubview(statusbarView)
-        
     }
-    
-    
+
     @objc func imageHistoryEvent (sender: UITapGestureRecognizer) {
         buttonHistoryLogEvent()
         isViewInteractionEnable(false)
@@ -278,7 +274,7 @@ class CameraViewController: BaseViewController, AVCapturePhotoCaptureDelegate {
             self.navigationController?.pushViewController(vc, animated: false)
         }
     }
-    
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         talkButtonImageView.isHidden = true
@@ -293,7 +289,7 @@ class CameraViewController: BaseViewController, AVCapturePhotoCaptureDelegate {
             cameraHistoryImageView.isHidden = false
             cameraHistoryImageView.image = self.cameraHistoryViewModel.cameraHistoryImages[0].image
         }
-        
+
         sessionQueue.async { [unowned self] in
             if self.sessionSetupSucceeds {
                 do {
@@ -324,7 +320,7 @@ class CameraViewController: BaseViewController, AVCapturePhotoCaptureDelegate {
         updateLanguageNames()
         zoomLevelGoogleAnalytic = 1.0
     }
-    
+
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(true)
         let tap = UITapGestureRecognizer(target: self, action: #selector(imageHistoryEvent(sender: )))
@@ -332,12 +328,11 @@ class CameraViewController: BaseViewController, AVCapturePhotoCaptureDelegate {
         captureButton.isExclusiveTouch = true
         PrintUtility.printLog(tag: TagUtility.sharedInstance.cameraScreenPurpose, text: "\(ScreenTracker.sharedInstance.screenPurpose)")
     }
-    
+
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         HomeViewController.cameraTapFlag = 0
         sessionQueue.async { [weak self] in
-            
             if let _ = self?.sessionSetupSucceeds {
                 if let _session = self?.session {
                     _session.stopRunning()
@@ -348,26 +343,26 @@ class CameraViewController: BaseViewController, AVCapturePhotoCaptureDelegate {
                                                   name: UIApplication.didBecomeActiveNotification,
                                                   object: nil)
     }
-    
+
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
         previewLayer.frame = previewView.layer.bounds
     }
-    
+
     func takePhoto(_ completion: ((Photo) -> Void)? = nil) {
         guard sessionSetupSucceeds else { return }
-        
+
         let settings: AVCapturePhotoSettings
         if self.photoOutput.availablePhotoCodecTypes.contains(.hevc) {
             settings = AVCapturePhotoSettings(format: [AVVideoCodecKey: AVVideoCodecType.hevc])
         } else {
             settings = AVCapturePhotoSettings()
         }
-        
+
         let orientation = videoOrientation
-        
+
         photoOutput.connection(with: .video)?.videoOrientation = orientation
-        
+
         let processor = PhotoCaptureProcessor()
         processor.orientation = orientation
         processor.completion = { [unowned self] processor in
@@ -404,7 +399,7 @@ class CameraViewController: BaseViewController, AVCapturePhotoCaptureDelegate {
             self.photoOutput.capturePhoto(with: settings, delegate: processor)
         }
     }
-    
+
     @objc
     private func handlePinch(_ pinch: UIPinchGestureRecognizer) {
         guard sessionSetupSucceeds,  let device = activeCamera else { return }
@@ -435,21 +430,21 @@ class CameraViewController: BaseViewController, AVCapturePhotoCaptureDelegate {
             return
         }
     }
-    
+
     @objc
     private func handleTap(_ tap: UITapGestureRecognizer) {
         guard sessionSetupSucceeds else { return }
-        
+
         let point = tap.location(in: previewView)
         let devicePoint = previewLayer.captureDevicePointConverted(fromLayerPoint: point)
-        
+
         configCamera(activeCamera) { device in
             let focusMode = AVCaptureDevice.FocusMode.autoFocus
             if device.isFocusPointOfInterestSupported && device.isFocusModeSupported(focusMode) {
                 device.focusPointOfInterest = devicePoint
                 device.focusMode = focusMode
             }
-            
+
             let exposureMode = AVCaptureDevice.ExposureMode.autoExpose
             if device.isExposurePointOfInterestSupported && device.isExposureModeSupported(exposureMode) {
                 device.exposurePointOfInterest = devicePoint
@@ -457,11 +452,9 @@ class CameraViewController: BaseViewController, AVCapturePhotoCaptureDelegate {
             }
         }
     }
-    
 }
 
 extension CameraViewController {
-    
     @IBAction func backButtonEventListener(_ sender: Any) {
         buttonBackLogEvent()
         isViewInteractionEnable(false)
@@ -470,18 +463,18 @@ extension CameraViewController {
         talkButtonImageView.isHidden = false
         NotificationCenter.default.post(name: .containerViewSelection, object: nil)
     }
-    
+
     var croppingParameters: CropUtils {
         return CropUtils(enabled: true, resizeable: true, dragable: true, minimumSize: minimumSize)
     }
-    
+
     @IBAction func captureButtonEventListener(_ sender: Any) {
         buttonCaptureLogEvent()
         if isCaptureButtonClickable == true {
             isViewInteractionEnable(false)
             isCaptureButtonClickable = false
             takePhoto { [self] photo in
-                
+
                 let image = photo.image()
                 let originalSize: CGSize
                 let width = previewView.frame.size.width
@@ -512,12 +505,11 @@ extension CameraViewController {
         cameraHistoryImageView.isUserInteractionEnabled = status
         captureButton.isUserInteractionEnabled = status
     }
-    
+
     internal func layoutCameraResult(uiImage: UIImage) {
         startConfirmController(uiImage: uiImage)
-        
     }
-    
+
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         ///Get the preview screen size to determine the focuspoint
         let screenSize = previewView.bounds.size
@@ -528,12 +520,12 @@ extension CameraViewController {
             let location = touchPoint.location(in: previewView)
             let focusCircleLocation = CGPoint(x: location.x, y: location.y)
             let backCamera = AVCaptureDevice.default(for: AVMediaType.video)
-            
+
             if let device = backCamera {
                 do {
                     try device.lockForConfiguration()
                     device.focusPointOfInterest = focusPoint
-                    
+
                     /// Pass the touch point of camera preview view as center point
                     pointInCamera(centerPoint: focusCircleLocation, circleColor: circleColor)
                     device.focusMode = .autoFocus
@@ -547,7 +539,7 @@ extension CameraViewController {
             }
         }
     }
-    
+
     func pointInCamera(centerPoint:CGPoint, circleColor: UIColor){
         let isInRegion = cameraPreviewView.frame.maxY > centerPoint.y + radius + radius
         if(isInRegion){
@@ -566,9 +558,7 @@ extension CameraViewController {
                 self.zoomLevel.isHidden = true
                 shapeLayer.removeFromSuperlayer()
             }
-
         }
-
     }
 
     private func startConfirmController(uiImage: UIImage) {
@@ -577,7 +567,7 @@ extension CameraViewController {
             defer {
                 self?.dismiss(animated: true, completion: nil)
             }
-            
+
             guard let image = image else {
                 return
             }
@@ -588,7 +578,7 @@ extension CameraViewController {
         self.view.window!.layer.add(transition, forKey: kCATransition)
         self.navigationController?.pushViewController(vc, animated: false)
     }
-    
+
     @IBAction func didTouchFlashButton(sender: UIButton) {
         let batteryPercentage = UIDevice.current.batteryLevel * batteryMaxPercent
         if batteryPercentage <= flashDisabledBatteryPercentage {
@@ -623,7 +613,7 @@ extension CameraViewController {
             }
         }
     }
-    
+
     func turnOffCamera() {
         do {
             if let activeCamera = activeCamera {
@@ -635,15 +625,13 @@ extension CameraViewController {
         } catch {
             PrintUtility.printLog(tag: "error", text: "")
         }
-
     }
-    
+
     func showLowBatteryErrorAlert(message: String){
         let alertService = CustomAlertViewModel()
         let alert = alertService.alertDialogWithoutTitleWithOkButtonAction(message: message) {}
         self.present(alert, animated: true, completion: nil)
     }
-    
 }
 
 //MARK: - Google analytics log events
